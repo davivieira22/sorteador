efe8a66719a9b491538c1355ad36d1b4beb7f083
davivieira22 <nftdavidavila22@gmail.com>
2024-03-12 23:02:11 -0300

    sorteador de numeros

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+
+const button=document.querySelector(".botao")
+const numero =document.querySelector(".resultado")
+
+
+
+
 function geradorDeNumero() {
+
 
     const min = Math.ceil(document.querySelector(".input-direito").value)
     const max = Math.floor(document.querySelector(".input-esquerdo").value)
     const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+  
 
+ 
+    if (min > max) {
+       numero.innerHTML=resultado
+    }else{
+        alert("O valor minimo precisa ser menor que o máximo!")  
+    }
 
-alert(resultado)
 
 }
+
+
